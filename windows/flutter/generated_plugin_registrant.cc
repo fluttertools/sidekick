feb5ef12bfcadb72b9022c004329ca50210bb510
leoafarias <leokcta@gmail.com>
2021-02-18 11:44:26 -0300

    Initial commit

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <file_chooser/file_chooser_plugin.h>
+#include <url_launcher_windows/url_launcher_plugin.h>
+#include <window_size/window_size_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileChooserPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
+}
