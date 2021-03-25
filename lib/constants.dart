5bc661d295cb6c6d40efbfcc1251cdf2a00ce557
leoafarias <leokcta@gmail.com>
2021-03-25 11:00:34 -0400

    Added link to release notes

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ const kNavigationWidth = 65.0;
 const kNavigationWidthExtended = 180.0;
 const kAvailableChannels = ['beta', 'dev', 'stable'];
 const kMasterChannel = 'master';
+
+const kFlutterTagsUrl = 'https://github.com/flutter/flutter/releases/tag/';
