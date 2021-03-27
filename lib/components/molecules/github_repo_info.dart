47c5b645e86d2ea397b4f65b5301ab0f6658094e
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-03-27 15:18:54 +0100

    add onPressed to Github info

--- lib/components/molecules/github_repo_info.dart
+++ lib/components/molecules/github_repo_info.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:github/github.dart';
 
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:sidekick/utils/open_link.dart';
 
 class GithubRepoInfo extends StatelessWidget {
   final Repository repo;
@@ -23,17 +24,26 @@ class GithubRepoInfo extends StatelessWidget {
       children: [
         const SizedBox(width: 10),
         TextButton.icon(
-          icon: Icon(Icons.star, size: 15),
+          onPressed: () {
+            openLink("${repo.htmlUrl}/stargazers");
+          },
+          icon: const Icon(Icons.star, size: 15),
           label: Text(repo.stargazersCount.toString()),
         ),
         const SizedBox(width: 10),
         TextButton.icon(
-          icon: Icon(MdiIcons.alertCircleOutline, size: 15),
+          onPressed: () {
+            openLink("${repo.htmlUrl}/issues");
+          },
+          icon: const Icon(MdiIcons.alertCircleOutline, size: 15),
           label: Text(repo.openIssuesCount.toString()),
         ),
         const SizedBox(width: 10),
         TextButton.icon(
-          icon: Icon(MdiIcons.sourceFork, size: 15),
+          onPressed: () {
+            openLink("${repo.htmlUrl}/network/members");
+          },
+          icon: const Icon(MdiIcons.sourceFork, size: 15),
           label: Text(repo.forksCount.toString()),
         ),
       ],
