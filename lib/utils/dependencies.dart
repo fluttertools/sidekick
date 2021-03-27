6be010502c892a65cc9348a680e5b09cfee5ce09
leoafarias <leokcta@gmail.com>
2021-03-27 09:04:51 -0400

    Updated dto and configured vscode

--- lib/utils/dependencies.dart
+++ lib/utils/dependencies.dart
@@ -7,6 +7,7 @@ import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:github/github.dart';
 import 'package:pub_api_client/pub_api_client.dart';
 import 'package:flutter_cache/flutter_cache.dart' as cache;
+import 'package:sidekick/dto/package_detail.dto.dart';
 
 const cacheKey = 'dependencies_cache_key';
 
@@ -16,71 +17,10 @@ final github = GitHub(
   auth: Authentication.anonymous(),
 );
 
-class PackageDetail {
-  @required
-  final String name;
-  @required
-  final String description;
-  @required
-  final String version;
-  @required
-  final String url;
-  @required
-  final String homepage;
-  @required
-  final String changelogUrl;
-  @required
-  final PackageScore score;
-  @required
-  final int count;
-  @required
-  final Repository repo;
-  PackageDetail({
-    this.name,
-    this.description,
-    this.version,
-    this.url,
-    this.homepage,
-    this.changelogUrl,
-    this.score,
-    this.count,
-    this.repo,
-  });
-
-  int compareTo(PackageDetail other) {
-    if (other.count > count) return -1;
-    if (other.count == count) return 0;
-    return 1;
-  }
-
-  PackageDetail.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        description = json['description'],
-        version = json['version'],
-        url = json['url'],
-        homepage = json['homepage'],
-        changelogUrl = json['changelogUrl'],
-        score = PackageScore.fromJson(json['score']),
-        count = json['count'],
-        repo = Repository.fromJson(json['repo']);
-
-  Map<String, dynamic> toJson() => {
-        'name': name,
-        'description': description,
-        'version': version,
-        'url': url,
-        'homepage': homepage,
-        'changelogUrl': changelogUrl,
-        'score': score,
-        'count': count,
-        'repo': repo,
-      };
-}
-
 Map<String, PubPackage> mapPackages;
 
 /// Fetches all packages info from pub.dev
-Future<List<PackageDetail>> fetchAllPackages(
+Future<List<PackageDetail>> fetchAllDependencies(
     Map<String, int> packagesCount) async {
   final response = await cache.remember(
     cacheKey,
