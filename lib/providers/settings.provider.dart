86fa883fdb1b6c72a3d76b6f446ce834d62fa5ad
leoafarias <leokcta@gmail.com>
2021-03-28 09:27:48 -0400

    Updated settings providers

--- lib/providers/settings.provider.dart
+++ lib/providers/settings.provider.dart
@@ -1,31 +1,44 @@
 import 'package:fvm/fvm.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sidekick/models/app_settings.model.dart';
+import 'package:sidekick/services/app_settings_service.dart';
 import 'package:state_notifier/state_notifier.dart';
 
-final settingsRepoProvider = Provider((_) => Settings());
+class AllSettings {
+  final AppSettings appSettings;
+  final FvmSettings fvmSettings;
+  AllSettings({this.appSettings, this.fvmSettings});
+}
+
+final settingsRepoProvider = Provider((_) => AllSettings());
 
 final settingsProvider = StateNotifierProvider<SettingsProvider>((ref) {
-  return SettingsProvider(initialState: Settings());
+  return SettingsProvider(initialState: AllSettings());
 });
 
-class SettingsProvider extends StateNotifier<Settings> {
-  SettingsProvider({Settings initialState}) : super(initialState) {
+class SettingsProvider extends StateNotifier<AllSettings> {
+  SettingsProvider({AllSettings initialState}) : super(initialState) {
     // Set initial settings from local storage
-    _init();
+    _load();
   }
 
-  Future<void> _init() async {
-    state = await Settings.read();
+  Future<void> _load() async {
+    final fvmSettings = await FvmSettings.read();
+    final appSettings = await AppSettingsService.read();
+    state = AllSettings(
+      appSettings: appSettings,
+      fvmSettings: fvmSettings,
+    );
   }
 
-  Future<Settings> read() async {
-    return state = await Settings.read();
-  }
+  // Future<AllSettings> read() async {
+  //   return state;
+  // }
 
-  Future<void> save(Settings settings) async {
-    state = settings;
-    await settings.save();
+  Future<void> save(AllSettings settings) async {
+    await settings.fvmSettings.save();
+    await AppSettingsService.save(settings.appSettings);
   }
 
   void reload() {
