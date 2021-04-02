1d00dc302f3cf6aba06b602114176dd66d61f837
leoafarias <leokcta@gmail.com>
2021-04-02 19:16:06 -0400

    Settings with icons

---
+++ lib/providers/theme.provider.dart
@@ -0,0 +1,3 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final themeProvider = StateProvider((ref) => {});
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:sidekick/components/atoms/screen.dart';
 import 'package:sidekick/providers/settings.provider.dart';
 import 'package:sidekick/screens/settings_scenes/settings_section_flutter.dart';
@@ -11,6 +12,12 @@ import 'package:sidekick/utils/notify.dart';
 import 'package:sidekick/utils/utils.dart';
 
 const sections = ['General', 'Projects', 'FVM', 'Flutter'];
+const sectionIcons = [
+  MdiIcons.tune,
+  MdiIcons.folderMultiple,
+  MdiIcons.hammerScrewdriver,
+  MdiIcons.console
+];
 
 typedef RenderSection = Widget Function(Settings, Function(void));
 
@@ -70,10 +77,15 @@ class SettingsScreen extends HookWidget {
                   children: sections.mapIndexed(
                     (section, idx) {
                       return ListTile(
+                        leading: Icon(
+                          sectionIcons[idx],
+                          size: 16,
+                        ),
                         title: Text(
                           section,
                           style: Theme.of(context).textTheme.subtitle1,
                         ),
+                        selectedTileColor: Theme.of(context).focusColor,
                         selected: currentSection.value == idx,
                         onTap: () => changeSection(idx),
                       );
@@ -82,6 +94,7 @@ class SettingsScreen extends HookWidget {
                 ),
               ),
             ),
+            const SizedBox(width: 20),
             Expanded(
               flex: 3,
               child: PageView(
