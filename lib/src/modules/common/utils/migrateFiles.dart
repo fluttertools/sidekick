538174f5a240081c81cf3c4396bd47d1035381ca
leoafarias <435833+leoafarias@users.noreply.github.com>
2021-06-22 16:39:34 -0400

    Added migration check

--- lib/src/modules/common/utils/migrateFiles.dart
+++ lib/src/modules/common/utils/migrateFiles.dart
@@ -1,14 +1,37 @@
 import 'dart:io';
 
-void migrateFiles(Directory oldPath, List<String> files, Directory newPath) {
-  files.forEach((fileName) {
-    final file =
-        File(oldPath.absolute.path + Platform.pathSeparator + fileName);
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final migrationName = 'hive_location_migration';
+
+Future<void> checkMigration(Directory newPath) async {
+  final prefs = await SharedPreferences.getInstance();
+
+  /// Return if has migrated
+  if (prefs.getBool(migrationName)) return;
+
+  final oldPath = await getApplicationDocumentsDirectory();
+  final files = [
+    'settings_box.lock',
+    'settings_box.hive',
+    'projects_service_box.lock',
+    'projects_service_box.hive'
+  ];
+
+  for (final fileName in files) {
+    final file = File(
+      oldPath.absolute.path + Platform.pathSeparator + fileName,
+    );
     if (file.existsSync()) {
       print('Legacy file found, starting migration...');
-      file.copySync(newPath.absolute.path + Platform.pathSeparator + fileName);
+      file.copySync(
+        newPath.absolute.path + Platform.pathSeparator + fileName,
+      );
       file.deleteSync();
       print('Done');
     }
-  });
+  }
+
+  await prefs.setBool(migrationName, true);
 }
