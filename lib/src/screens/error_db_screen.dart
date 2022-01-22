15ae28c97a44ac843c90c086e0da4adb45a8ac2a
Eduardo M <contact@eduardom.dev>
2022-01-22 12:39:35 +0100

    Add movable tragets for error DB screen

--- lib/src/screens/error_db_screen.dart
+++ lib/src/screens/error_db_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:sidekick/src/modules/../components/atoms/empty_dataset.dart';
 import 'package:sidekick/src/modules/common/utils/open_link.dart';
+import 'package:sidekick/src/windowBorder.dart';
+import 'package:window_manager/window_manager.dart';
 
 class ErrorDBScreen extends StatelessWidget {
   const ErrorDBScreen({Key key}) : super(key: key);
@@ -12,70 +15,95 @@ class ErrorDBScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Container(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  EmptyDataset(
-                    icon: Icon(Icons.error_outline_outlined),
-                    iconColor: Theme.of(context).errorColor,
-                    opacity: 1,
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Text(
-                    'There was an isssue opening Sidekick',
-                    style: Theme.of(context).textTheme.headline4,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    text,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
+        body: Stack(
+          children: [
+            Center(
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
                     children: [
-                      TextButton.icon(
-                        style: ButtonStyle(
-                            padding: MaterialStateProperty.resolveWith(
-                                (states) => EdgeInsets.symmetric(
-                                    horizontal: 20, vertical: 10))),
-                        onPressed: () {
-                          openLink(context,
-                              'https://github.com/leoafarias/sidekick/issues/new/choose');
-                        },
-                        label: Text('Create new issue'),
-                        icon: Icon(
-                          Icons.open_in_new,
-                        ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      EmptyDataset(
+                        icon: Icon(Icons.error_outline_outlined),
+                        iconColor: Theme.of(context).errorColor,
+                        opacity: 1,
                       ),
                       SizedBox(
-                        width: 10,
+                        height: 50,
                       ),
-                      ElevatedButton.icon(
-                        label: Text('Close'),
-                        icon: const Icon(Icons.close),
-                        onPressed: () {
-                          exit(0);
-                        },
+                      Text(
+                        'There was an isssue opening Sidekick',
+                        style: Theme.of(context).textTheme.headline4,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        text,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextButton.icon(
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.resolveWith(
+                                    (states) => EdgeInsets.symmetric(
+                                        horizontal: 20, vertical: 10))),
+                            onPressed: () {
+                              openLink(context,
+                                  'https://github.com/leoafarias/sidekick/issues/new/choose');
+                            },
+                            label: Text('Create new issue'),
+                            icon: Icon(
+                              Icons.open_in_new,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          ElevatedButton.icon(
+                            label: Text('Close'),
+                            icon: const Icon(Icons.close),
+                            onPressed: () {
+                              exit(0);
+                            },
+                          ),
+                          SizedBox(
+                            height: 50,
+                          ),
+                        ],
                       )
                     ],
-                  )
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTapDown: (_) {
+                        windowManager.startDragging();
+                      },
+                    ),
+                  ),
+                  WindowButtons(),
                 ],
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
