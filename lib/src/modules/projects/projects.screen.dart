015a05e0d0c352da086a97219ac52f4c14a7986d
Eduardo M <contact@eduardom.dev>
2022-05-07 21:00:08 +0200

    Lint fixes

--- lib/src/modules/projects/projects.screen.dart
+++ lib/src/modules/projects/projects.screen.dart
@@ -1,5 +1,4 @@
 import 'package:file_selector/file_selector.dart' as selector;
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
