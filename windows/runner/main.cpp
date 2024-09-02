e3c4ab11b35faf19f5a64d90073454a8f3c3fbe2
arce lai <zainai311@qq.com>
2024-09-02 22:00:32 +0800

    feat(native): signle-window-instance

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -10,6 +10,12 @@ auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP);
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command)
 {
+    HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", L"Sidekick");
+    if (hwnd != NULL) {
+        ::ShowWindow(hwnd, SW_NORMAL);
+        ::SetForegroundWindow(hwnd);
+        return EXIT_FAILURE;
+    }
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
